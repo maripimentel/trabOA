ef786ce0c5f7e84176076da2d427ef7496dc6194
diceEviscerator <yurifleury12@gmail.com>
2017-04-27 16:44:09 -0300

    função de seleção do menu inicializada, structs da especificação incluidas na biblioteca

--- include/drive_lib.h
+++ include/drive_lib.h
@@ -1,4 +1,30 @@
 #include <stdio.h>
 #include <conio.h>
 
+typedef struct block {
+	unsigned char bytes_s[512];
+} block;
+
+typedef struct  sectror_array {
+	block sector[60];
+} sectror_array;
+
+typedef struct track_array {
+	sectror_array track[5];
+} track_array;
+
+typedef struct  fatlist_s {
+	unsigned int used;
+	unsigned int eof;
+	unsigned int next;
+} fatlist;
+
+typedef struct struct  fatent_s {
+	char file_name[100];
+	unsigned int first_sector;
+} fatent;
+
+
 void show_menu ();
+
+void menu_selection (int menu_option);
--- src/my_drive.cpp
+++ src/my_drive.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 #include "disk_lib.h"
 
-void show_menu () {
+void show_menu () { // simples função que limpa a tela e mostra o menu.
 
   clrscr;
   printf("
@@ -19,9 +19,40 @@ void show_menu () {
   return;
 }
 
-int main (){
+void menu_selection (int menu_option) { // função que direciona o programa para as funções dependendo da opção escolhida.
 
+  switch (menu_option){
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    default:
+      break;
+  }
+  return;
+} 
+
+int main (){
+  int menu_option=0;
 
   show_menu;
+  scanf("%d", &menu_option);
+  while ((menu_option!=1)
+      &&(menu_option!=2)
+      &&(menu_option!=3)
+      &&(menu_option!=4)
+      &&(menu_option!=5)) { // garantia de entrada correta;
+    show_menu;
+    printf("Escolha invalida!\nEntre novamente.\n");
+    scanf("%d", &menu_option);
+  }
+  menu_selection;
+
 
 }
